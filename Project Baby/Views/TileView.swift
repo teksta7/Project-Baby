d085d6adc261a211af3f601e6caa5b16560916c8
teksta7 <b688nf@yahoo.co.uk>
2024-08-15 01:28:38 +0100

    Initial Structure

---
+++ Project Baby/Views/TileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProjectTabView.swift
+//  Project Baby
+//
+//  Created by Jake thompson on 15/08/2024.
+//
+
+import SwiftUI
+
+//Create custom Tile
+struct TileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TileView()
+}
